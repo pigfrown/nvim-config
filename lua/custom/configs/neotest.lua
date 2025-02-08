9ad4f8aeb5db355a0820c54245a66fb34f4ae43d
Pig Frown <craig.lang@samsonvt.com>
2025-02-08 12:18:48 +0000

    feat: testing improvements

--- lua/custom/configs/neotest.lua
+++ lua/custom/configs/neotest.lua
@@ -1,10 +1,17 @@
 local neotest = require 'neotest'
 
 neotest.setup {
+  icons = {
+    passed = '🎉',
+    running = '⏳',
+    failed = '😭',
+    skipped = '➖',
+    unknown = '❓',
+  },
   adapters = {
     require 'neotest-jest' {
       jestCommand = 'npm test --',
-      jestConfigFile = 'custom.jest.config.ts',
+      jestConfigFile = 'jest.config.ts',
       env = { CI = true },
       cwd = function()
         return vim.fn.getcwd()
@@ -12,3 +19,23 @@ neotest.setup {
     },
   },
 }
+
+vim.keymap.set('n', '<leader>tt', function()
+  neotest.run.run()
+end, { desc = 'Run [T]ests selected' })
+
+vim.keymap.set('n', '<leader>ta', function()
+  neotest.run.run { suite = true }
+end, { desc = 'Run [A] tests' })
+
+vim.keymap.set('n', '<leader>tl', function()
+  neotest.run.run_last()
+end, { desc = 'Run [L]ast Test' })
+
+vim.keymap.set('n', '<leader>tD', function()
+  neotest.run.run_last { strategy = 'dap', suite = false }
+end, { desc = '[D]ebug Last Test' })
+
+vim.keymap.set('n', '<leader>tw', function()
+  neotest.run.run { jestCommand = 'npx jest --watch', suite = false }
+end, { desc = '[W]atch tests' })
